ec1dd21e4c011547477f92e115f333f87484c44c
Wurielle <noahfenghom@gmail.com>
2017-10-30 17:58:12 +0100

    Config done

---
+++ assets/src/front/js/main.ts
@@ -0,0 +1,28 @@
+// Sass imports
+import '../sass/main.sass';
+
+// JS imports -- Allows you to see what I used for this project (Also check those from webpack.ProvidePlugin() in 'webpack.config.js')
+import 'gsap';
+import 'gsap/ScrollToPlugin';
+import 'gsap/Draggable';
+import * as $ from 'jquery';
+import * as backbone from 'backbone';
+import * as _ from 'underscore';
+
+// Set imports to the Global object
+declare global {
+    interface Window {
+        $: any;
+        _: any;
+        backbone: any;
+    }
+}
+window.$ = $;
+window._ = _;
+window.backbone = backbone;
+
+// JS
+$(function() {
+    console.log('app ready');
+    $("body").css({'background-color': '#eaeaea'});
+});
---
+++ webpack.config.js
@@ -0,0 +1,162 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractToCSS = new ExtractTextPlugin({
+    filename: "./css/[name].css",
+    disable: false
+});
+module.exports = {// See https://webpack.js.org/concepts/
+    devtool: 'source-map',
+    target: 'node',
+    context: path.resolve(__dirname, './'),
+    entry: {
+        main: './assets/src/front/js/main.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, './assets/dist/'),
+        publicPath: "../",
+        filename: './js/[name].bundle.js',
+    },
+    module: {
+        rules: [// See: https://webpack.js.org/configuration/module/#rule, https://webpack.js.org/concepts/loaders/
+        { // Typescript loader
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        },
+        {// Sass loader
+            test: /\.sass$/,
+            use: extractToCSS.extract({
+                use: [{
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: function () {
+                            return [
+                                require('autoprefixer')
+                            ];
+                        },
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: "resolve-url-loader"
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        indentedSyntax: 'sass',
+                        sourceMap: true
+                    }
+                }],
+                fallback: "style-loader"
+            })
+        },
+        {// Stylus loader
+            test: /\.styl$/,
+            use: extractToCSS.extract({
+                use: [{
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: function () {
+                            return [
+                                require('autoprefixer')
+                            ];
+                        },
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: "resolve-url-loader"
+                },
+                {
+                    loader: "stylus-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }],
+                fallback: "style-loader"
+            })
+        },
+        {// CSS loader
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: [{
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: function () {
+                            return [
+                                require('autoprefixer')
+                            ];
+                        },
+                        sourceMap: true
+                    }
+                }],
+                fallback: "style-loader"
+            }),
+        },
+        {// Mustache loader
+            test: /\.html$/,
+            use: "mustache-loader"
+        },
+        {// Image loader (Resolving Images path)
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            exclude: [/fonts/],
+            use: [{
+                loader:'file-loader',
+                options: {
+                    name: 'images/[name].[ext]'
+                }
+            }]
+
+        },
+        {// Font loader
+            test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]'
+                }
+            }]
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        alias: {
+            "jquery-mobile": "jquery-mobile/dist/jquery.mobile" //webpack couldn't find jquery-mobile so I set the path myself
+        },
+    },
+    plugins: [
+        require('autoprefixer'),
+        extractToCSS, // see first few line to see the definition and the output
+        new webpack.ProvidePlugin({
+          $ : "jquery",
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery',
+          Backbone : "backbone",
+          _ : "underscore",
+      }),
+      new webpack.NoEmitOnErrorsPlugin(),
+    ]
+};
+
+// Tuto: https://blog.madewithenvy.com/getting-started-with-webpack-2-ed2b86c68783
+// Doc: https://webpack.js.org
+
+// NOTE: const is like var
+
+// NOTE: __dirname refers to the directory where this webpack.config.js lives, which in this blogpost is the project root.
+
+// NOTE: in the output, [name] stands for the entry name of your entry, in this case it's main
+
+// NOTE: queries are now options so if you have something like: {loader: 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'}, you can turn it into {loader: 'image-webpack-loader', options: {bypassOnDebug: true, optimizationLevel: 7, interlaced: false}
+// See https://webpack.js.org/guides/migrating/#what-are-options-
