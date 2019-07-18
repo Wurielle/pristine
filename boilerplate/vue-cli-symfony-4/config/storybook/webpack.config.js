122df80e82c83b4183835e385f2a2f7f0121e9ed
Nowa <noahfenghom@gmail.com>
2019-07-18 16:01:42 +0200

    Update webpack.config.js
    fixed storybook not working with typescript

--- boilerplate/vue-cli-symfony-4/config/storybook/webpack.config.js
+++ boilerplate/vue-cli-symfony-4/config/storybook/webpack.config.js
@@ -4,11 +4,24 @@ const webpack = require('webpack');
 const DSSPlugin = require('@bit/wurielle.pristine.webpack.dss-plugin');
 const JsonSassPlugin = require('@bit/wurielle.pristine.webpack.json-sass-plugin');
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    appendTsSuffixTo: ['\\.vue$']
+                }
+            }
+        ]
+    },
     resolve: {
         alias: {
             '@config':  path.resolve(__dirname, '../../config'),
             '@':  path.resolve(__dirname, '../../assets/app'),
-        }
+        },
+        extensions: ['.tsx', '.ts', '.js', '.vue']
     },
     plugins:[
         new DSSPlugin({
@@ -16,6 +29,6 @@ module.exports = {
             output: './assets/app/styleguide.json',
             watch: './assets/app'
         }),
-        new JsonSassPlugin('./config/theme.js', './config/theme.scss'),
+        new JsonSassPlugin('./config/tailwind.config.js', './config/tailwind.config.scss'),
     ]
 };
