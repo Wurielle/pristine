70faa43d8dd0bcbfe1d46ca0b0ca2e150fd8c097
Nowa <noahfenghom@gmail.com>
2020-06-08 10:29:14 +0200

    Update pristine.ts

--- src/pristine.ts
+++ src/pristine.ts
@@ -1,23 +1,20 @@
-/* Auto Imports */
-[
-    '@',
-    '@root/templates',
-
-].forEach((path) => {
-    const src = require.context(path, true, /\.(runtime|asset)\.(.*?)$/);
-    src.keys().forEach(src);
-});
+/* Import Pristine Config (for HRM)  */
+import '@config/pristine.config.js';
 
 /* Add polyfills from the polyfills folder (order matters) */
 import '@/polyfills/WeakMap';
 import '@/polyfills/MutationObserver';
 import '@/polyfills/ObjectFit';
 
-/* Import Pristine Config (for HRM)  */
-import '@config/pristine.config.js';
-
 /** Unicons: https://iconscout.com/unicons */
 import '@iconscout/unicons/css/unicons.css';
 
 /* Import Styles */
 import '@/styles/main.scss';
+
+/* Auto Imports */
+const srcContext = require.context('@', true, /\.(runtime|asset)\.(.*?)$/);
+srcContext.keys().forEach(srcContext);
+
+const templatesContext = require.context('@root/templates', true, /\.(runtime|asset)\.(.*?)$/);
+templatesContext.keys().forEach(templatesContext);
